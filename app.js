3f232058fc7562de31682f305ceea5bc11245026
Lavínia Madeira <132295407+Laviniamadeira@users.noreply.github.com>
2025-02-06 10:46:11 -0300

    realizei alguns cometarios ao decorrer do código para vizualizar melhor como irei começar " git: 'commitrealizei alguns cometarios ao decorrer do código para vizualizar melhor como irei começar '

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos=[]
+//selecionar campo para escrever o nome e o nome que foi escrito vai ser adicionado na lista amigos (tem que liberar o botão adicionar)
